27ba17479ce142c8596ba8b2813b83ed9392ac80
Vaughn Kottler <vaughn@libre-embedded.com>
2025-07-01 09:57:11 -0500

    5.15.5 - add 'hide' default filter

--- runtimepy/__init__.py
+++ runtimepy/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=3.2.3
-# hash=b0f0463a508c9a1312a071f5c0b06f3b
+# hash=8ce7abe0f8c0e64d9afc0b5e4de6e2ad
 # =====================================
 
 """
@@ -10,7 +10,7 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "A framework for implementing Python services."
 PKG_NAME = "runtimepy"
-VERSION = "5.15.4"
+VERSION = "5.15.5"
 
 # runtimepy-specific content.
 METRICS_NAME = "metrics"

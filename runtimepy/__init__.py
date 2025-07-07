170043298bb96e5ec60ec00e819a6ddd8c52eb15
v <vaughn@libre-embedded.com>
2025-07-06 19:01:19 -0500

    5.15.6 - Upgrades for html directory viewer (#335)

--- runtimepy/__init__.py
+++ runtimepy/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=3.2.3
-# hash=8ce7abe0f8c0e64d9afc0b5e4de6e2ad
+# hash=08c1dda8bd5fefe13af6a8eb49063498
 # =====================================
 
 """
@@ -10,7 +10,7 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "A framework for implementing Python services."
 PKG_NAME = "runtimepy"
-VERSION = "5.15.5"
+VERSION = "5.15.6"
 
 # runtimepy-specific content.
 METRICS_NAME = "metrics"

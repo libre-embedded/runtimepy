0e0caad5292c58d05445c902acd36cfa1fcd37d6
Vaughn Kottler <vaughn@libre-embedded.com>
2026-03-18 17:18:22 -0500

    5.17.5 - telemetry fix

--- runtimepy/__init__.py
+++ runtimepy/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=3.2.4
-# hash=60e85fa9b62652ef5c1d40a9eb86598b
+# hash=266513d1d4703356e49ab54f32ba2359
 # =====================================
 
 """
@@ -10,7 +10,7 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "A framework for implementing Python services."
 PKG_NAME = "runtimepy"
-VERSION = "5.17.4"
+VERSION = "5.17.5"
 
 # runtimepy-specific content.
 METRICS_NAME = "metrics"
